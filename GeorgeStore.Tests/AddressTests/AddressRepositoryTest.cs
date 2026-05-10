085c3b6ca6d3b4b85f580aeafa90d48947bf2fba
Jorge Gerardo <jorgegerardo0105@gmail.com>
2026-05-10 09:45:59 -0700

    Refactor Address repository and add Bogus to add fake data

--- GeorgeStore.Tests/AddressTests/AddressRepositoryTest.cs
+++ GeorgeStore.Tests/AddressTests/AddressRepositoryTest.cs
@@ -1,8 +1,9 @@
-﻿using GeorgeStore.Features.Addresses;
+﻿using Bogus;
+using GeorgeStore.Common.Shared;
+using GeorgeStore.Features.Addresses;
 using GeorgeStore.Features.Users;
 using GeorgeStore.Infrastructure.Data;
 using GeorgeStore.Tests.Common;
-using GeorgeStore.Common.Shared;
 using Moq;
 
 namespace GeorgeStore.Tests.AddressTests;
@@ -13,51 +14,35 @@ public class AddressRepositoryTest
     public async Task SetAsDefaultTest()
     {
         using var context = ContextHelper.Create();
-        var dbConnection = new Mock<IDbConnectionFactory>();
-        User user = CreateUser(context);
-        AddressRepository addressRepository = new(context, dbConnection.Object);
-
-        var workAdds = Address.Create(user.Id, "Work", "", "", "", "", "", "", "", "", true);
-        var homeAdds = Address.Create(user.Id, "Home", "", "", "", "", "", "", "", "", false);
-        var MomHomAdds = Address.Create(user.Id, "Mom's house", "", "", "", "", "", "", "", "", false);
-
-        user.Addresses = [workAdds, homeAdds, MomHomAdds];
-
-        context.Users.Update(user);
-        context.SaveChanges();
-
-        Result result = await addressRepository.SetAsDefaultAsync(user.Id, homeAdds.Id);
+        AddressRepository addressRepository = CreateRepository(context);
+        User user = ContextHelper.CreateUser(context);
+        Address addr1 = CreateRandomAddress(context, user, true);
+        Address addr2 = CreateRandomAddress(context, user, false);
+        Address addr3 = CreateRandomAddress(context, user, false);
 
         //Act
+        Result result = await addressRepository.SetAsDefaultAsync(user.Id, addr2.Id);
+        //Assert
         Assert.True(result.IsSuccess);
-        Assert.Single(user.Addresses, a => a.IsDefault);
-        Assert.False(workAdds.IsDefault);
-        Assert.False(MomHomAdds.IsDefault);
-        Assert.True(homeAdds.IsDefault);
+        Address defaultAddress = Assert.Single(user.Addresses, a => a.IsDefault);
+        Assert.Equal(defaultAddress.Id, addr2.Id);
     }
 
     [Fact]
     public async Task SetAsDefaultTest_AddressNotFound()
     {
         using var context = ContextHelper.Create();
-        var dbConnection = new Mock<IDbConnectionFactory>();
-        User user = CreateUser(context);
-        AddressRepository addressRepository = new(context, dbConnection.Object);
-
-        var workAdds = Address.Create(user.Id, "Work", "", "", "", "", "", "", "", "", true);
-        var homeAdds = Address.Create(user.Id, "Home", "", "", "", "", "", "", "", "", false);
-
-        user.Addresses = [workAdds, homeAdds];
-
-        context.Users.Update(user);
-        context.SaveChanges();
-
+        User user = ContextHelper.CreateUser(context);
+        AddressRepository addressRepository = CreateRepository(context);
+        Address addr1 = CreateRandomAddress(context, user, true);
+        Address addr2 = CreateRandomAddress(context, user, false);
 
         //Act
-        context.Addresses.Remove(homeAdds);
+        context.Addresses.Remove(addr2);
         context.SaveChanges();
-        Result result = await addressRepository.SetAsDefaultAsync(user.Id, homeAdds.Id);
+        Result result = await addressRepository.SetAsDefaultAsync(user.Id, addr2.Id);
 
+        //Assert
         Assert.False(result.IsSuccess);
         Assert.Equal(AddressError.NotFound, result.Error);
     }
@@ -66,23 +51,17 @@ public class AddressRepositoryTest
     public async Task RemoveTest()
     {
         using var context = ContextHelper.Create();
-        var dbConnection = new Mock<IDbConnectionFactory>();
-        User user = CreateUser(context);
-        AddressRepository addressRepository = new(context, dbConnection.Object);
+        User user = ContextHelper.CreateUser(context);
+        AddressRepository addressRepository = CreateRepository(context);
 
-        var workAdds = Address.Create(user.Id, "Work", "", "", "", "", "", "", "", "", true);
-        var homeAdds = Address.Create(user.Id, "Home", "", "", "", "", "", "", "", "", false);
-        var MomHomAdds = Address.Create(user.Id, "Mom's house", "", "", "", "", "", "", "", "", false);
+        var addr1 = CreateRandomAddress(context, user, true);
+        var addr2 = CreateRandomAddress(context, user, false);
+        var addr3 = CreateRandomAddress(context, user, false);
 
-        user.Addresses = [workAdds, homeAdds, MomHomAdds];
-
-        context.Users.Update(user);
-        context.SaveChanges();
-
-        Result result = await addressRepository.RemoveAsync(user.Id, homeAdds.Id);
+        Result result = await addressRepository.RemoveAsync(user.Id, addr2.Id);
         Assert.True(result.IsSuccess);
         Assert.Equal(2, user.Addresses.Count);
-        Assert.DoesNotContain(user.Addresses, a => a.Id == homeAdds.Id);
+        Assert.DoesNotContain(user.Addresses, a => a.Id == addr2.Id);
 
     }
 
@@ -90,22 +69,16 @@ public class AddressRepositoryTest
     public async Task Remove_AddressNotFound()
     {
         using var context = ContextHelper.Create();
-        var dbConnection = new Mock<IDbConnectionFactory>();
-        User user = CreateUser(context);
-        AddressRepository addressRepository = new(context, dbConnection.Object);
-
-        var homeAdds = Address.Create(user.Id, "Home", "", "", "", "", "", "", "", "", false);
-        var MomHomAdds = Address.Create(user.Id, "Mom's house", "", "", "", "", "", "", "", "", false);
-
-        user.Addresses = [homeAdds, MomHomAdds];
-
-        context.Users.Update(user);
+        User user = ContextHelper.CreateUser(context);
+        AddressRepository addressRepository = CreateRepository(context);
+        Address addr1 = CreateRandomAddress(context, user, false);
+        Address addr2 = CreateRandomAddress(context, user, false);
+        user.Addresses.Remove(addr1);
         context.SaveChanges();
 
         //Act
-        user.Addresses.Remove(homeAdds);
-        context.SaveChanges();
-        Result result = await addressRepository.RemoveAsync(user.Id, homeAdds.Id);
+        Result result = await addressRepository.RemoveAsync(user.Id, addr1.Id);
+        //Assert
         Assert.False(result.IsSuccess);
         Assert.Equal(AddressError.NotFound, result.Error);
 
@@ -115,60 +88,53 @@ public class AddressRepositoryTest
     public async Task Add()
     {
         using var context = ContextHelper.Create();
-        var dbConnection = new Mock<IDbConnectionFactory>();
-        User user = CreateUser(context);
-        AddressRepository addressRepository = new(context, dbConnection.Object);
+        User user = ContextHelper.CreateUser(context);
+        AddressRepository addressRepository = CreateRepository(context);
         const string defaultAddressAlias = "Work";
-        AddressCreateDto request = new("Home", "Street", "123", "Los Mochis", "Sinaloa", "81200", "MX", "Jorge", "6681234567", false);
-        AddressCreateDto request2 = new(defaultAddressAlias, "Street", "123", "Los Mochis", "Sinaloa", "81200", "MX", "Jorge", "6681234567", true);
+        AddressCreateDto request1 = CreateRandomAddressDto(isDefault: false);
+        AddressCreateDto request2 = CreateRandomAddressDto(isDefault: true, alias: defaultAddressAlias);
 
         //Act
-        Result result1 = await addressRepository.AddAsync(user.Id, request);
+        Result result1 = await addressRepository.AddAsync(user.Id, request1);
         Result result2 = await addressRepository.AddAsync(user.Id, request2);
-
+        //Assert
         Assert.True(result1.IsSuccess);
         Assert.True(result2.IsSuccess);
         Assert.Equal(2, user.Addresses.Count);
-        Assert.Single(user.Addresses, a => a.IsDefault);
-        Address? defaultAddress = user.Addresses.FirstOrDefault(a => a.IsDefault);
-        Assert.NotNull(defaultAddress);
+        Address defaultAddress = Assert.Single(user.Addresses, a => a.IsDefault);
         Assert.Equal(defaultAddressAlias, defaultAddress.Alias);
     }
 
     [Fact]
-    public async Task Add_WithoutDefaultAddress()
+    public async Task Add_NoOneIsDefault()
     {
         using var context = ContextHelper.Create();
-        var dbConnection = new Mock<IDbConnectionFactory>();
-        User user = CreateUser(context);
-        AddressRepository addressRepository = new(context, dbConnection.Object);
-        const string defaultAddressAlias = "Work";
-        AddressCreateDto request = new("Home", "Street", "123", "Los Mochis", "Sinaloa", "81200", "MX", "Jorge", "6681234567", false);
-        AddressCreateDto request2 = new(defaultAddressAlias, "Street", "123", "Los Mochis", "Sinaloa", "81200", "MX", "Jorge", "6681234567", false);
+        User user = ContextHelper.CreateUser(context);
+        AddressRepository addressRepository = CreateRepository(context);
+        AddressCreateDto request1 = CreateRandomAddressDto(isDefault: false);
+        AddressCreateDto request2 = CreateRandomAddressDto(isDefault: false);
 
         //Act
-        Result result1 = await addressRepository.AddAsync(user.Id, request);
+        Result result1 = await addressRepository.AddAsync(user.Id, request1);
         Result result2 = await addressRepository.AddAsync(user.Id, request2);
-
+        //Assert
         Assert.True(result1.IsSuccess);
         Assert.True(result2.IsSuccess);
         Assert.Equal(2, user.Addresses.Count);
-        Address? defaultAddress = user.Addresses.FirstOrDefault(a => a.IsDefault);
-        Assert.Null(defaultAddress);
+        Assert.DoesNotContain(user.Addresses, a => a.IsDefault);
     }
 
     [Fact]
     public async Task Add_LimitedReached()
     {
         using var context = ContextHelper.Create();
-        var dbConnection = new Mock<IDbConnectionFactory>();
-        User user = CreateUser(context);
-        AddressRepository addressRepository = new(context, dbConnection.Object);
+        User user = ContextHelper.CreateUser(context);
+        AddressRepository addressRepository = CreateRepository(context);
         const string defaultAddressAlias = "Work";
-        AddressCreateDto request1 = new("Home", "Street", "123", "Los Mochis", "Sinaloa", "81200", "MX", "Jorge", "6681234567", true);
-        AddressCreateDto request2 = new("Home2", "Street", "123", "Los Mochis", "Sinaloa", "81200", "MX", "Jorge", "6681234567", true);
-        AddressCreateDto request3 = new(defaultAddressAlias, "Street", "123", "Los Mochis", "Sinaloa", "81200", "MX", "Jorge", "6681234567", true);
-        AddressCreateDto request4 = new("Forest", "Street", "123", "Los Mochis", "Sinaloa", "81200", "MX", "Jorge", "6681234567", true);
+        AddressCreateDto request1 = CreateRandomAddressDto(isDefault: true);
+        AddressCreateDto request2 = CreateRandomAddressDto(isDefault: true);
+        AddressCreateDto request3 = CreateRandomAddressDto(isDefault: true, alias:  defaultAddressAlias);
+        AddressCreateDto request4 = CreateRandomAddressDto(isDefault: true);
 
         //Act
         Result result1 = await addressRepository.AddAsync(user.Id, request1);
@@ -176,6 +142,7 @@ public class AddressRepositoryTest
         Result result3 = await addressRepository.AddAsync(user.Id, request3);
         Result result4 = await addressRepository.AddAsync(user.Id, request4);
 
+        //Assert
         Assert.True(result1.IsSuccess);
         Assert.True(result2.IsSuccess);
         Assert.True(result3.IsSuccess);
@@ -184,19 +151,58 @@ public class AddressRepositoryTest
         Assert.Equal(AddressError.LimitReached, result4.Error);
 
         Assert.Equal(3, user.Addresses.Count);
-        Assert.Single(user.Addresses, a => a.IsDefault);
-        Address? defaultAddress = user.Addresses.FirstOrDefault(a => a.IsDefault);
+        Address defaultAddress = Assert.Single(user.Addresses, a => a.IsDefault);
         Assert.NotNull(defaultAddress);
         Assert.Equal(defaultAddressAlias, defaultAddress.Alias);
     }
 
 
     //Common arranges
-    private static User CreateUser(GeorgeStoreContext context)
+    private static AddressRepository CreateRepository(GeorgeStoreContext context)
     {
-        User user = new("Jorguito", "jorguito@gmail.com");
-        context.Add(user);
+        var dbConnection = new Mock<IDbConnectionFactory>();
+        return new(context, dbConnection.Object);
+    }
+
+    private static AddressCreateDto CreateRandomAddressDto(bool isDefault = false, string? alias = null)
+    {
+        var faker = new Faker("es_MX");
+
+        return new AddressCreateDto(
+            alias is null 
+                ? faker.PickRandom("Home", "Work", "Mom's House", "Office")
+                : alias,
+            faker.Address.StreetAddress(),
+            faker.Address.County(),
+            faker.Address.City(),
+            faker.Address.State(),
+            faker.Address.ZipCode(),
+            faker.Address.BuildingNumber(),
+            faker.Random.Bool()
+                ? faker.Random.Number(1, 20).ToString()
+                : null,
+            faker.Lorem.Sentence(),
+            isDefault
+        );
+    }
+    private static Address CreateRandomAddress(GeorgeStoreContext context, User user, bool isDefault = false)
+    {
+        var faker = new Faker("es_MX");
+        Address newAddr = Address.Create(
+            user.Id, 
+            faker.Address.StreetName(), 
+            faker.Address.StreetAddress(),
+            faker.Address.County(),
+            faker.Address.City(), 
+            faker.Address.State(),
+            faker.Address.ZipCode(),
+            faker.Address.BuildingNumber(),
+            faker.Random.Bool() ? faker.Random.Number(1, 20).ToString() : null,
+            faker.Lorem.Sentence(),
+            isDefault
+        );
+        context.Add(newAddr);
         context.SaveChanges();
-        return user;
+        return newAddr;
     }
 }
