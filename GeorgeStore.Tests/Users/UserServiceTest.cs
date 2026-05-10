ebf2cd5344f9b2689aafd36722be3e7d8ef2b027
Jorge Gerardo <jorgegerardo0105@gmail.com>
2026-05-09 22:03:58 -0700

    user-test refactor finished

--- GeorgeStore.Tests/Users/UserServiceTest.cs
+++ GeorgeStore.Tests/Users/UserServiceTest.cs
@@ -13,15 +13,30 @@ public class UserServiceTest
     {
         using var context = ContextHelper.Create();
         User user = CreateUser(context);
-        var userManager = CreateUserManager(user.Id, "jorguito@gmail.com", user);
+        var userManager = CreateUserManager(user);
         UserService userSrv = new(userManager);
 
         //Act
         var result = await userSrv.Login("juanita@gmail.com", "Password123!");
-
-
+        //Assert
         Assert.False(result.IsSuccess);
         Assert.Equal(UserError.Notfound, result.Error);
+        Assert.Throws<InvalidOperationException>(() => _ = result.Value);
+    }
+
+    [Fact]//TODO: Pending
+    public async Task Login_Failure_InvalidCredentials()
+    {
+        using var context = ContextHelper.Create();
+        User user = CreateUser(context);
+        var userManager = CreateUserManager(user);
+        UserService userSrv = new(userManager);
+
+        //Act
+        var result = await userSrv.Login(user.Email!, "IcorrectPassword123!");
+        //Assert
+        Assert.False(result.IsSuccess);
+        Assert.Equal(UserError.InvalidCredentials, result.Error);
         Assert.Throws<InvalidOperationException>(() => _ = result.Value);
     }
 
@@ -29,13 +44,12 @@ public class UserServiceTest
     public async Task GetProfile_UserNotFound()
     {
         using var context = ContextHelper.Create();
-        const string email = "jorguito@gmail.com";
         User user = CreateUser(context);
-        var userManager = CreateUserManager(user.Id, email, user);
+        var userManager = CreateUserManager(user);
         UserService userSrv = new(userManager);
-
+        //Act
         var result = await userSrv.GetProfile(Guid.NewGuid());
-
+        //Assert
         Assert.False(result.IsSuccess);
         Assert.Equal(UserError.Notfound, result.Error);
         Assert.Throws<InvalidOperationException>(() => _ = result.Value);
@@ -45,38 +59,37 @@ public class UserServiceTest
     public async Task GetProfile()
     {
         using var context = ContextHelper.Create();
-        const string email = "jorguito@gmail.com";
         User user = CreateUser(context);
-        var userManager = CreateUserManager(user.Id, email, user);
+        var userManager = CreateUserManager(user);
         UserService userSrv = new(userManager);
-
+        //Act
         var result = await userSrv.GetProfile(user.Id);
-
+        //Assert
         Assert.True(result.IsSuccess);
-        var profileDto = result.Value;
-        Assert.NotNull(profileDto);
-        Assert.Equal(email, profileDto.Email);
+        UserDataDto profileDto = result.Value;
+        Assert.Equal(user.Email!, profileDto.Email);
+        Assert.Equal(user.UserName, profileDto.UserName);
     }
 
     [Fact]
     public async Task ExistTest()
     {
         using var context = ContextHelper.Create();
-        const string email = "jorguito@gmail.com";
         User user = CreateUser(context);
-        var userManager = CreateUserManager(user.Id, email, user);
+        var userManager = CreateUserManager(user);
         UserService userSrv = new(userManager);
 
-
-        var result = await userSrv.Exist(email);
+        //Act
+        var result = await userSrv.Exist(user.Email!);
+        //Assert
         Assert.True(result.IsSuccess);
     }
 
     //Common arranges
-    private static UserManager<User> CreateUserManager(Guid userId, string email, User user)
+    private static UserManager<User> CreateUserManager(User user)
     {
         var store = new Mock<IUserStore<User>>();
-        var passwordHasher = new PasswordHasher<User>();
+        PasswordHasher<User> passwordHasher = new();
         var userManager = new Mock<UserManager<User>>(store.Object, null!, passwordHasher, null!, null!, null!, null!, null!, null!);
         userManager.Setup(u => u.FindByEmailAsync(user.Email!)).ReturnsAsync(user);
         userManager.Setup(u => u.FindByIdAsync(user.Id.ToString())).ReturnsAsync(user);
