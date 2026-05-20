68e1a13d20dafe4e5e2f7db005aac81cc1a543ce
Jorge Gerardo <jorgegerardo0105@gmail.com>
2026-05-20 09:05:02 -0700

    Fix arquitecture

---
+++ GeorgeStore/Admin/EasyData/EasyDataController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GeorgeStore.Admin.EasyData;
+
+[Route("easydata")]
+public class EasyDataController : Controller
+{
+    [Route("{**entity}")]
+    public IActionResult Index(string entity) =>
+        View("~/Admin/EasyData/Index.cshtml");
+}
